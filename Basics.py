0633a009ef98812b70c4299d6c89e105083e83fd
John Kimberl <148263397+JohnKimberl@users.noreply.github.com>
2024-12-23 00:17:29 -0500

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,1 +1,23 @@
+user= input("Enter Your Name:")
+if user.isdigit():
+  print(f"Hello {user}")
+else: 
+  print("Enter a valid name")
+  
+operator= input ("Enter a operator (+ - * /):")
+num1 = float(input("Enter the first number: "))
+num2= float(input("Enter the second number: "))
 
+if operator == "+":
+    result = num1 +num2
+elif operator == "-":
+    result = num1- num2
+elif operator == "*":
+    result = num1*num2
+elif operator == "/":
+    result = num1/num2
+else:
+    print(f"{operator} is not a valid operator")
+
+print (f"\n Hello your operation gives the result of {result:.2f}. Thank you for using the program")
+    
